2e62e74078805e1192f544dba84b58af2fb59d42
olim99 <olimchik074@gmail.com>
2025-02-25 15:39:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const body = document.querySelector('body')
+
+const swiper = new Swiper('.swiper', {
+    
+    direction: 'horizontal',
+    loop: true,
+
+
+    
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+      
+
+    on: {
+        slideChange: function () {
+            let activeSlide = document.querySelector('.swiper-slide-active');
+            if(activeSlide) {
+                let bgColor = activeSlide.getAttribute('data-bg');
+                body.style.backgroundColor = bgColor;
+            }
+        }
+    }
+});
